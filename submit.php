7f999b0f89f776a01e12aa2780d0e9dcb1f3734d
Priyanka-345 <104905382+Priyanka-345@users.noreply.github.com>
2022-05-31 19:24:19 +0530

    submit.php

--- submit.php
+++ submit.php
@@ -1,12 +1,12 @@
 <?php
-require_once 'connection.php';
+require_once 'config.php';
 $Username = $_POST['username'];
 $Password = $_POST['password'];
 $Email = $_POST['email'];
-$rno = $_POST['rollnumber'];
+$rno = $_POST['id'];
 $Message = $_POST['msg'];
 
-$insert = "insert into form(username , password,email,rollnumber,Msg) values ('$Username','$Password','$Email','$rno','$Message')";
+$insert = "insert into form(username , password,email,id,Msg) values ('$Username','$Password','$Email','$rno','$Message')";
 $run_query = mysqli_query($conn,$insert);
 if($run_query)
 {
